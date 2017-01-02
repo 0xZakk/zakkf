d54aff8715d82589e0366400c5ec6a3a043c64ca
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2017-01-02 08:01:38 -0500

    add node env to index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 const fs = require('fs')
 
-fs.readFile('./info.json', 'utf8', ( err, data ) => {
+fs.readFile(__dirname + '/info.json', 'utf8', ( err, data ) => {
   if ( err ) {
     console.log( err )
   }
